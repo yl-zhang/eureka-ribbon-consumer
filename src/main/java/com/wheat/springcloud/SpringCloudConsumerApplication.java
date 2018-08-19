84ec339f78e91ac04ab365149350e7cfba05b5f7
张月楼 YuelouZhang <yuelouzhang@synacast.local>
2018-08-19 10:21:23 +0800

    eureka-consumer

--- src/main/java/com/wheat/springcloud/SpringCloudConsumerApplication.java
+++ src/main/java/com/wheat/springcloud/SpringCloudConsumerApplication.java
@@ -3,19 +3,29 @@ package com.wheat.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableDiscoveryClient
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringCloudConsumerApplication {
-	
+
 	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder){
+	@LoadBalanced
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
 	
+	@Bean
+	public IRule randomRule() {
+	    return new RandomRule();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConsumerApplication.class, args);
 	}
